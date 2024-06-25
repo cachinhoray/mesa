a600051056973b462fcd73f25719f4d01ee3f79c
cachinhoray <135022506+cachinhoray@users.noreply.github.com>
2024-06-25 11:44:38 -0300

    aa

--- src/app/duvidas/page.jsx
+++ src/app/duvidas/page.jsx
@@ -15,12 +15,12 @@ export default function Duvidas() {
         <p>Em 2024, durante o desenvolvimento do nosso Trabalho de Conclusão de Curso (TCC), nos inspiramos em criar um sistema de iluminação e tomada inteligente utilizando a plataforma Arduino e Internet das Coisas.</p>
         <p>Essa ideia nos levou a criar uma mesa de Pinball única, que combina a emoção do jogo com a inovação da tecnologia.</p>
         <div className="image-container">
-          <Image width={1000} height={1000} src={pinballg} alt="Mesa de Pinball" />
+          <Image width={250} height={250} src={pinballg} alt="Mesa de Pinball" />
         </div>
         <p>A nossa mesa de Pinball é equipada com sensores, LEDs e motores que trabalham em conjunto para criar uma experiência de jogo única.</p>
         <p>Com a nossa tecnologia, você pode controlar a iluminação, a música e os efeitos especiais da mesa de Pinball remotamente, utilizando um aplicativo móvel.</p>
         <div className="image-container">
-          <Image width={1000} height={1000} src={pinballm} alt="Mesa de Pinball" />
+          <Image width={240} height={240} src={pinballm} alt="Mesa de Pinball" />
         </div>
         <p>Agora, queremos compartilhar essa história com você e mostrar como a nossa paixão pelo Pinball pode inspirar a criação de algo incrível.</p>
         <p>Se você está interessado em saber mais sobre a nossa tecnologia e como ela pode ser utilizada em outros projetos, entre em contato conosco!</p>
---
+++ src/app/homelogin/page.jsx
@@ -0,0 +1,90 @@
+// pages/compra.js
+
+import Head from 'next/head';
+import styles from '../styles/page.module.css';
+
+export default function Compra() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Tela de Compra</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="styles.css" />
+      </Head>
+
+      <main>
+        
+        <h1>Finalizar Compra</h1>
+        
+        <form action="#" className={styles['checkout-form']}>
+          <div className={styles['form-group']}>
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required />
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="endereco">Endereço de Entrega:</label>
+            <textarea id="endereco" name="endereco" required></textarea>
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="produto">Produto:</label>
+            <select id="produto" name="produto">
+              <option value="produto1">Produto 1</option>
+              <option value="produto2">Produto 2</option>
+              <option value="produto3">Produto 3</option>
+            </select>
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="quantidade">Quantidade:</label>
+            <input type="number" id="quantidade" name="quantidade" min="1" required />
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="plano">Plano:</label>
+            <select id="plano" name="plano">
+              <option value="basico">Básico</option>
+              <option value="profissional">Profissional</option>
+              <option value="empresarial">Empresarial</option>
+              <option value="extraPlus">Extra Plus</option>
+            </select>
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="formaPagamento">Forma de Pagamento:</label>
+            <select id="formaPagamento" name="formaPagamento">
+              <option value="cartaoCredito">Cartão de Crédito</option>
+              <option value="boleto">Boleto</option>
+              <option value="paypal">PayPal</option>
+              <option value="pix">PIX</option>
+            </select>
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf" required />
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="telefone">Telefone:</label>
+            <input type="tel" id="telefone" name="telefone" required />
+          </div>
+
+          <div className={styles['form-group']}>
+            <label htmlFor="cep">CEP:</label>
+            <input type="text" id="cep" name="cep" required />
+          </div>
+
+          <button type="submit">Finalizar Compra</button>
+        </form>
+      </main>
+    </div>
+  );
+}
